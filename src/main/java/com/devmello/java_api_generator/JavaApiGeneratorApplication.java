820d248be233042a8287923a5f4821594f9f3ce7
Mello <sbrunomello@gmail.com>
2025-03-02 11:42:08 -0300

    first commit

---
+++ src/main/java/com/devmello/java_api_generator/JavaApiGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.devmello.java_api_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaApiGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaApiGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devmello/java_api_generator/JavaApiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmello.java_api_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
